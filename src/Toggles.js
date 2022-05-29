93ace42c87940a9050dc68a58f313b16a7662d20
Alun Turner <alun.turner@googlemail.com>
2022-05-29 09:36:04 +0100

    read props in Toggles, add prop types

--- src/Toggles.js
+++ src/Toggles.js
@@ -1,7 +1,20 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
 import "./Toggles.css";
 
-function Toggles() {
-  return <h1>React Toggles here</h1>;
+function Toggles({ question, answers }) {
+  return <h1>{question}</h1>;
 }
+
+Toggles.propTypes = {
+  question: PropTypes.string.isRequired,
+  answers: PropTypes.arrayOf(
+    PropTypes.shape({
+      correctChoiceIndex: PropTypes.number,
+      choices: PropTypes.arrayOf(PropTypes.string),
+    })
+  ).isRequired,
+};
 
 export default Toggles;
