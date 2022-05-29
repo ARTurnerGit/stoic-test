328d81116e5f4c35cdc761ac5c2712ba86e739b2
Alun Turner <alun.turner@googlemail.com>
2022-05-29 13:42:05 +0100

    add styling for 3 option list

--- src/Toggles.js
+++ src/Toggles.js
@@ -5,19 +5,44 @@ import classnames from "classnames";
 import ToggleChoice from "./ToggleChoice";
 import "./Toggles.css";
 
+function getRandomIndexForArray(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
 function generateDisplayAnswers(answers) {
-  const mappedAnswers = answers.map((a) => ({ ...a, currentChoiceIndex: 1 }));
+  const mappedAnswers = answers.map((a) => ({
+    ...a,
+    currentChoiceIndex: getRandomIndexForArray(a.choices),
+  }));
+
   const randomisedAnswers = [];
   while (mappedAnswers.length > 0) {
-    const randomIndex = Math.floor(Math.random() * mappedAnswers.length);
+    const randomIndex = getRandomIndexForArray(mappedAnswers);
     randomisedAnswers.push({ ...mappedAnswers[randomIndex] });
     mappedAnswers.splice(randomIndex, 1);
   }
+
+  // final check to make sure we don't randomly load all answers correctly
+  if (
+    randomisedAnswers.every(
+      ({ correctChoiceIndex, currentChoiceIndex }) =>
+        correctChoiceIndex === currentChoiceIndex
+    )
+  ) {
+    const indexToChange = getRandomIndexForArray(randomisedAnswers);
+    const answerToChange = randomisedAnswers[indexToChange];
+    if (answerToChange.correctChoiceIndex === 0) {
+      answerToChange.currentChoiceIndex = 1;
+    } else {
+      answerToChange.currentChoiceIndex = 0;
+    }
+  }
+
   return randomisedAnswers;
 }
 
 function Toggles({ question, answers }) {
-  const [displayAnswers, setDisplayAnswers] = useState(
+  const [displayAnswers, setDisplayAnswers] = useState(() =>
     generateDisplayAnswers(answers)
   );
 
@@ -49,7 +74,7 @@ function Toggles({ question, answers }) {
 
   return (
     <div
-      className={classnames("toggleContainer", {
+      className={classnames("togglesContainer", {
         percentage0: percentageCorrect >= 0,
         percentage50: percentageCorrect >= 0.5,
         percentage100: percentageCorrect === 1,
